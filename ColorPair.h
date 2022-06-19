8717fc3aaed2673d73abb7190d7c931497c226b9
deepasn08 <srrajesh.svpur@gmail.com>
2022-06-19 16:33:21 +0530

    cechink github user config

--- ColorPair.h
+++ ColorPair.h
@@ -3,6 +3,10 @@
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
+
+
+
+
 const char* MajorColorNames[] = {
     "White", "Red", "Black", "Yellow", "Violet"
 };

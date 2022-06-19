2278e399507ae4541ded71eab91bf718265d8dcf
deepasn08 <deepareddy997@gmail.com>
2022-06-19 18:47:20 +0530

    adding testcase for pair no 9 and 10

--- main.c
+++ main.c
@@ -18,8 +18,19 @@ int main() {
     testNumberToPair(4, WHITE, BROWN);
     testNumberToPair(5, WHITE, SLATE);
 
+    testNumberToPair(9, RED, BROWN);
+    testNumberToPair(10, RED, SLATE);
+
     testPairToNumber(BLACK, ORANGE, 12);
     testPairToNumber(VIOLET, SLATE, 25);
+
+
+
+    /* testing collectiondata
+    const ColorPair colors = {.majorColor = 0, .minorColor = 0};
+    //const struct ColorPair* colorsAddress = &colors ;
+    char* buffer;
+    ColorPairToString( &colors, buffer);*/
 
     return 0;
 }

0dc5a34892784d1de975dab426d89c1791a128fd
deepasn08 <deepareddy997@gmail.com>
2022-06-19 17:13:49 +0530

    variable declarations are updated

--- main.c
+++ main.c
@@ -2,6 +2,18 @@
 #include "assert.h"
 #include "ColorPair.h"
 
+const char* MajorColorNames[] = {
+    "White", "Red", "Black", "Yellow", "Violet"
+};
+int numberOfMajorColors =
+    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+const char* MinorColorNames[] = {
+    "Blue", "Orange", "Green", "Brown", "Slate"
+};
+const int MAX_COLORPAIR_NAME_CHARS = 16;
+int numberOfMinorColors =
+    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
 int main() {
     testNumberToPair(4, WHITE, BROWN);
     testNumberToPair(5, WHITE, SLATE);
